c4dc6e6c96bc5bbd749a066a7813352e7ff3eabe
snake <admin@viasnake.com>
2018-01-24 20:52:18 +0900

    Footer 追加
    内容不十分
    取り敢えずデザインの固定

--- index.php
+++ index.php
@@ -64,5 +64,8 @@
                 ?>
             </div>
         </div>
+        <?php
+        include dirname(__FILE__) . '/template/footer.php';
+        ?>
     </body>
 </html>
--- template/footer.php
+++ template/footer.php
@@ -1,8 +1,11 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div class="footer">
+    <div class="container">
+        <center>
+            <!-- Copyright -->
+            <p>
+                &copy; <?php echo date('Y'); ?> <a href="/">www.virtualyoutuber.net</a><br>
+                Created by <a href="https://twitter.com/intent/user?user_id=802271220">@SNAKE0053</a><br>
+            </p>
+        </center>
+    </div>
+</div>
