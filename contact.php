a58e62ef6fb4ee4255377927bf54d2ae1e9b96f0
snake <admin@viasnake.com>
2018-01-25 00:08:35 +0900

    contact.php追加
    問い合わせページ
    適当な調整

---
+++ contact.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <?php
+    define("title", "RULE");
+    include dirname(__FILE__) . '/template/header.php';
+    ?>
+    <body>
+        <?php
+        include dirname(__FILE__) . '/template/nav.php';
+        include dirname(__FILE__) . '/template/widget/message.php';
+        ?>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdUI4-VYc6BSAFo0EPkgSKZcapAx4K4n2KJDF8WaNaOqu1-7w/viewform?embedded=true" width="720" height="1200" frameborder="0" marginheight="0" marginwidth="0">
+                        読み込んでいます...
+                    </iframe>
+                </div>
+                <div class="col-md-4">
+                    <?php
+                    include dirname(__FILE__) . '/template/sidebar.php';
+                    ?>
+                </div>
+            </div>
+        </div>
+        <?php
+        include dirname(__FILE__) . '/template/footer.php';
+        ?>
+    </body>
+</html>
