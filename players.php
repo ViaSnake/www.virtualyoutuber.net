780a6e54ef535f6292652acf050996558f3bb218
snake <admin@viasnake.com>
2018-01-24 23:31:55 +0900

    players.php追加
    後々修正はいるかも

---
+++ players.php
@@ -0,0 +1,52 @@
+<?php
+$json = file_get_contents(dirname(__FILE__) . "/data/players.json");
+$players = json_decode($json, true);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <?php
+    define("title", "PLAYERS");
+    include dirname(__FILE__) . '/template/header.php';
+    ?>
+    <body>
+        <?php
+        include dirname(__FILE__) . '/template/nav.php';
+        include dirname(__FILE__) . '/template/widget/message.php';
+        ?>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h3>参加選手一覧</h3>
+                    <p>登録順</p>
+                    <?php foreach ($players as $key => $value) { ?>
+                        <div class="card text-center">
+                            <h5 class="card-header">
+                                <?php echo $value["name"]; ?>
+                            </h5>
+                            <div class="card-body">
+                                <p class="card-text">
+                                    YouTube - <?php echo '<a href="https://www.youtube.com/channel/' . $value["youtubechannel_id"] . '">' . $value["youtubechannel_name"] . '</a>'; ?><br>
+                                    Twitter - <?php echo '<a href="https://twitter.com/intent/user?user_id=' . $value["twitter_id"] . '">' . $value["twitter_screenname"] . '</a>'; ?><br>
+                                    VTuber歴 - <?php echo $value["history"]; ?><br>
+                                </p>
+                                <h5>意気込み</h5>
+                                <?php echo $value["comment"]; ?><br>
+                            </div>
+                        </div>
+                        <br>
+                    <?php } ?>
+                </div>
+                <div class="col-md-4">
+                    <?php
+                    include dirname(__FILE__) . '/template/sidebar.php';
+                    ?>
+                </div>
+            </div>
+        </div>
+        <?php
+        include dirname(__FILE__) . '/template/footer.php';
+        ?>
+    </body>
+</html>
