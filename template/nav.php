0ab74b788fe5a818db9c95d68bae78c8abb668a5
snake <admin@viasnake.com>
2018-01-24 21:14:14 +0900

    navの修正諸々
    navのセンタリング
    その他ファイルの修正？

--- template/nav.php
+++ template/nav.php
@@ -1,25 +1,24 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">HOME</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">RULE</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">PLAYERS</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">LINK</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">CONTACT</a>
-      </li>
-    </ul>
-  </div>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">HOME</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">RULE</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">PLAYERS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">LINK</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">CONTACT</a>
+            </li>
+        </ul>
+    </div>
 </nav>
