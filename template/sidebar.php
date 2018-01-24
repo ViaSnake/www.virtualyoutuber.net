6a94d68133cc73c2b05f01a3338d7a207c7bcaa8
snake <admin@viasnake.com>
2018-01-24 22:05:42 +0900

    navとsidebar修正
    nav リンク修正
    sidebar col箇所おかしいの修正

--- template/sidebar.php
+++ template/sidebar.php
@@ -1,14 +1,12 @@
-<div class="col-md-4">
-    <?php
-    include dirname(__FILE__) . '/template/widget/banner.php';
-    ?>
-    <div class="card text-center">
-        <div class="card-header">
-            公式Twitter
-        </div>
-        <div class="card-block">
-            <a class="twitter-timeline" data-height="700" data-theme="light" data-link-color="#981CEB" href="https://twitter.com/vtuber_net?ref_src=twsrc%5Etfw">Tweets by vtuber_net</a>
-            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-        </div>
+<?php
+include dirname(__FILE__) . '/template/widget/banner.php';
+?>
+<div class="card text-center">
+    <div class="card-header">
+        公式Twitter
+    </div>
+    <div class="card-block">
+        <a class="twitter-timeline" data-height="700" data-theme="light" data-link-color="#981CEB" href="https://twitter.com/vtuber_net?ref_src=twsrc%5Etfw">Tweets by vtuber_net</a>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
 </div>
