476a11dcc779b06a39e6e07c14f421a0a07ebe9a
SNAKE <snake@gravittogroup.xyz>
2018-01-24 20:12:47 +0900

    sidebarとmessageをincへ
    運用側の負担減らすために簡単にメッセージ出せるように今後するよう

---
+++ inc/message.php
@@ -0,0 +1,6 @@
+<div class="alert alert-danger" role="alert">
+    <center>エントリー締切日 - 2018/02/14(日)</center>
+</div>
+<div class="alert alert-info" role="alert">
+    <center>現在のエントリー人数 - 7名</center>
+</div>
---
+++ inc/sidebar.php
@@ -0,0 +1,15 @@
+<!-- Sidebar -->
+<div class="col-md-4">
+    <?php
+    include dirname(__FILE__) . '/inc/banner.php';
+    ?>
+    <div class="card text-center">
+        <div class="card-header">
+            公式Twitter
+        </div>
+        <div class="card-block">
+            <a class="twitter-timeline" data-height="700" data-theme="light" data-link-color="#981CEB" href="https://twitter.com/vtuber_net?ref_src=twsrc%5Etfw">Tweets by vtuber_net</a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+    </div>
+</div>
