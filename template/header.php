3a8f075fbcab353649a58470cfed603c8ec93137
snake <admin@viasnake.com>
2018-01-24 21:39:31 +0900

    index.php周辺の修正
    custom.cssの編集
    
    cssの読み込み順が頭悪かったのを修正

--- template/header.php
+++ template/header.php
@@ -3,10 +3,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>第一回バーチャルユーチューバー選手権 | <?php echo title; ?></title>
-    <!-- CustomのCSS読み込み -->
-    <link href="/css/custom.css" rel="stylesheet">
     <!-- BootstrapのCSS読み込み -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <!-- CustomのCSS読み込み -->
+    <link href="/css/custom.css" rel="stylesheet">
     <!-- jQueryのJS読み込み -->
     <script src="/js/jquery-3.2.1.min.js" async></script>
 </head>
