b5497f9fc09b7e223b55d88b820097e75b3acef5
SNAKE <snake@gravittogroup.xyz>
2018-01-25 23:39:48 +0900

    デザイン修正
    col-md-8 ・ col-md-4から
    col-md-10 ・ col-md-2へ変更

--- players.php
+++ players.php
@@ -17,7 +17,7 @@ $players = json_decode($json, true);
 
         <div class="container">
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-10">
                     <h3>参加選手一覧</h3>
                     <p>登録順</p>
                     <?php foreach ($players as $key => $value) { ?>
@@ -44,7 +44,7 @@ $players = json_decode($json, true);
                         <br>
                     <?php } ?>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-2">
                     <?php
                     include dirname(__FILE__) . '/template/sidebar.php';
                     ?>
