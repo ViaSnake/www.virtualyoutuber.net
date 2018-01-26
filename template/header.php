d63b79fe35fb5da876eda0732b0fa641c2138107
SNAKE <snake@gravittogroup.xyz>
2018-01-26 23:24:32 +0900

    js追加
    足りていなかったjs追加

--- template/header.php
+++ template/header.php
@@ -3,10 +3,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <title>第一回バーチャルユーチューバー選手権 | <?php echo title; ?></title>
+    <meta name="description" content="バーチャルユーチューバーの頂点に立つのは誰だ！第一回バーチャルユーチューバー選手権公式ウェブサイト">
+
     <!-- BootstrapのCSS読み込み -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <!-- CustomのCSS読み込み -->
     <link href="/css/custom.css" rel="stylesheet">
     <!-- jQueryのJS読み込み -->
     <script src="/js/jquery-3.2.1.min.js" async></script>
+    <!-- popperのJS読み込み -->
+    <script src="/js/popper.min.js" async></script>
+    <!-- BootstrapのJS読み込み -->
+    <script src="/js/bootstrap.min.js" async></script>
 </head>
