153babc8d382a5e86a4b660470db07b7bb45992c
snake <admin@viasnake.com>
2018-01-26 23:24:13 +0900

    nav修正
    nav修正

--- template/nav.php
+++ template/nav.php
@@ -1,8 +1,5 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarSupportedContent">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="collapse navbar-collapse d-flex justify-content-center" id="navbar">
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link" href="/">HOME</a>
