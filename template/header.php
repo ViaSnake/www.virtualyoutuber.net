d243343594bdf2f04a7e75e00759bec3a4931ef7
SNAKE <snake@gravittogroup.xyz>
2018-01-27 14:17:26 +0900

    scriptのasync削除
    挙動を不安定化させていた原因なので削除

--- template/header.php
+++ template/header.php
@@ -9,10 +9,11 @@
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <!-- CustomのCSS読み込み -->
     <link href="/css/custom.css" rel="stylesheet">
+    
     <!-- jQueryのJS読み込み -->
-    <script src="/js/jquery-3.2.1.min.js" async></script>
+    <script src="/js/jquery-3.2.1.min.js"></script>
     <!-- popperのJS読み込み -->
-    <script src="/js/popper.min.js" async></script>
+    <script src="/js/popper.min.js"></script>
     <!-- BootstrapのJS読み込み -->
-    <script src="/js/bootstrap.min.js" async></script>
+    <script src="/js/bootstrap.min.js"></script>
 </head>
