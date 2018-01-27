270cf62bb5d9166086acbc40c50a2847fe8b2501
SNAKE <snake@gravittogroup.xyz>
2018-01-27 16:14:09 +0900

    <main>タグへ置き換え

--- rule.php
+++ rule.php
@@ -10,7 +10,7 @@
         include dirname(__FILE__) . '/template/message.php';
         ?>
 
-        <div class="container">
+        <main class="container">
             <div class="row">
                 <div class="col-md-10">
                     <h1>ルール</h1>
@@ -98,7 +98,7 @@
                     ?>
                 </div>
             </div>
-        </div>
+        </main>
     </body>
     <?php
     include dirname(__FILE__) . '/template/footer.php';
