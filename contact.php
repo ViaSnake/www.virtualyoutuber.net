27a60165a0aa2327768112b9b656cf6820b10e61
snake <admin@viasnake.com>
2018-01-27 13:53:28 +0900

    contact.phpのレスポンシブ対応
    Close #3

--- contact.php
+++ contact.php
@@ -14,9 +14,11 @@
             <div class="row">
                 <div class="col-md-10">
                     <h1>CONTACT</h1>
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdUI4-VYc6BSAFo0EPkgSKZcapAx4K4n2KJDF8WaNaOqu1-7w/viewform?embedded=true" width="720" height="1200" frameborder="0" marginheight="0" marginwidth="0">
-                        読み込んでいます...
-                    </iframe>
+                    <div class="embed-responsive embed-responsive-1by1">
+                        <iframe class="embed-responsive-item" src="https://docs.google.com/forms/d/e/1FAIpQLSdUI4-VYc6BSAFo0EPkgSKZcapAx4K4n2KJDF8WaNaOqu1-7w/viewform?embedded=true" width="720" height="1200" frameborder="0" marginheight="0" marginwidth="0">
+                            読み込んでいます...
+                        </iframe>
+                    </div>
                 </div>
                 <div class="col-md-2">
                     <?php
