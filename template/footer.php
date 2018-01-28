05c57f96c785e8ba348ce2acb465ba5676ce0cea
snake <admin@viasnake.com>
2018-01-28 14:54:59 +0900

    RepositoryのURL追加

--- template/footer.php
+++ template/footer.php
@@ -3,8 +3,9 @@
         <center>
             <!-- Copyright -->
             <p>
-                &copy; <?php echo date('Y'); ?> <a href="/">www.virtualyoutuber.net</a><br>
+                &copy;<?php echo date('Y'); ?> <a href="/">www.virtualyoutuber.net</a><br>
                 Created by <a href="https://twitter.com/intent/user?user_id=802271220">@SNAKE0053</a><br>
+                <a href="https://github.com/snake0623/www.virtualyoutuber.net/">Github repository</a><br>
             </p>
         </center>
     </div>
